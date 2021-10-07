0c73b990e472682e6f693bf9db359c6d85e0ae53
swapnalikolte <92086369+swapnalikolte@users.noreply.github.com>
2021-10-07 14:50:27 +0530

    Add files via upload

---
+++ if .py
@@ -0,0 +1,182 @@
+# 1
+# # A=25
+# # B=35
+# # C=15
+# # if(A>B):
+# #     print("A is greater number")
+# # elif(B>C):
+# #     print("B is greater number")
+# # else:
+# #     print("c is greater number") 
+
+# # print()           
+# 2
+# # num=int(input("enter a number"))
+# # Num=int(input("enter a number"))
+# # if(Num>num):
+# #     print(Num,"is maximum number")
+# # else:
+# #     print(" num is not maximum number")    
+
+
+# 3
+# # A=int(input("enter a number"))
+# # B=int(input("enter a number"))
+# # C=int(input("enter a number"))
+# # if(A>B):
+# #     print("A is greater number")
+# # elif(B>C):
+# #     print("B is greater number")
+# # elif(C>A):
+# #     print("C is greater number ")
+# # else:
+# #     print(" number is equal")   
+
+# 4
+# # a=int(input("enter any number"))
+# # if a>0:
+# #    print("the number is positive ")
+# # elif a<0:
+# #    print("the number is negative ")
+# # else:
+# #     print("the number is zero")     
+
+# 5
+# # number=int(input("enter any positive number"))
+# # if(number%5==0) and (number%11==0):
+# #     print("given number is divisible by 5 and 11")
+# # else:
+# #     print("given number is not divisible by 5 and 11")    
+
+# 6
+# # a=int(input("enter a number"))
+# # if(a%2==0):
+# #     print("the number is even")
+# # else:
+# #     print("number is odd")  
+
+# # 7
+# # year=int(input("enter a year"))
+# # if(year%4==0):
+# #    print("this year is leap  year")
+# # elif(year%400==0):
+# #     print("this year is leap  year")
+# # elif(year%100==0):
+# #     print("this year is leap  year")
+# # else:
+# #     print("year is not leap year")    
+
+# # 8
+# # char=(input("enter any alphabet :"))
+# # if(char>='a' and char<='z') or (char>='A' and char<='Z'):
+# #   print("The character is an alphabet")
+# # else:
+# #     print("The character is not an alphabet")
+# # 8
+# # # char=input("enter any charater")
+# # # if(char=='a' or char=='e' or char=='i' or  char=='o' or char=='u'):
+# # #     print("the character is ovel")
+# # # else:
+# # #     print("character is not ovel")
+
+# # 9
+# # # char=input("enter any alphabet")
+# # # if(char>='A' and char<='Z'):
+# # #     print("the alphabet is in upper case")
+# # # else:
+# # #     print("the alphabet is in lower case")
+
+# # day=input("enter any day\n")
+# # if(day=="monday"):
+# #     print("rajma chawal")
+# # elif(day=="tuesday"):
+# #      print("pav bhaji") 
+# # elif(day=="wednesday"):
+# #        print("biryani")
+# # elif(day=="thursday"):
+# #   print("palak panir")
+# # elif(day=="friday"):
+# #    print("chole bature")
+# # elif(day=="saturday"):
+# #    print("litti chokka")
+# # else:
+# #    print("poha")
+
+    
+
+
+          
+
+
+# # salary=int(input("enter your salary:"))
+# # years=int(input("enter years of service"))
+# # if(years>5):
+# #   print("your salary(+bonus)=",salary+(salary)*5/100)
+# # else:
+# #   print("no bonus")
+
+# # length=int(input("enter length"))
+# # breadth=int(input("enter breadth"))
+# # if(length==breadth):
+# #   print("it is square")
+# # else:
+# #   print("it is not a square")  
+
+# # num1=input("enter first number")
+# # num2=input("enter second number")
+# # if(num1>num2):
+# #   print("first number is greatest")
+# # elif(num1<num2):
+# #   print("second number is greatest")
+# # else:
+# #   print("both number are equal")
+
+
+# # num=int(input("enter the choice 1 to 12"))
+# # sum=0
+# # if temp==num:
+# #   print(num,"is armstrong number")
+# # else:
+# #   print(num," is not armstrong number")
+
+
+# # room=int(input("enter the number"))
+# # if room==12:
+# #   print("no beds")
+# # elif( room<12):
+# #    print("need girls")
+# # else:
+# #    print("less girl")
+
+
+# # num=int(input("enter a number"))
+# # if num%5==0:
+# #    print("hello")
+# # else:
+# #    print("bye")   
+
+# # amt=0
+# # nu=int(input("enter number electric unit"))
+# # if nu<=100:
+# #   amt=0
+# # if nu>100 and nu<=200:   
+
+num1=int(input("enter your first number:"))
+num2=int(input("enter your second number:"))
+oper=(input("enter any operator:"))
+if oper=='+' or oper=="addition":
+   print(num1+num2)
+elif oper=='-' or oper=="substraction":
+   print(num1-num2)
+elif oper =='*' or oper=="multiplication":
+   print(num1*num2)
+elif oper=='/' or oper== "division":
+   print(num1 / num2)
+else:
+   print("enter valid operator")
+
+# num=int(input("enter any number :"))
+# print("the last digit of number :",num%10)
+
+
+       
